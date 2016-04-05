97dc323fe0e63f1267acc1a94aae7c85784ad2a5
“Benjamski” <your_email@example.com>
2016-04-05 11:33:36 -0400

    Working on sign in HTML/css FIRST will commit when seemingly functional templatesa are built in front end before working on sign in feautues at all.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,3 +1,37 @@
 'use strict';
 
 const app = require('../app-data');
+
+const signUp = (success, failure, data) => {
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/sign-up',
+    data
+
+  }).done(success)
+    .fail(failure);
+
+};
+
+const signIn = (success, failure, data) => {
+  $.ajax({
+    method: 'POST',
+    url: app.api + '/sign-in',
+    data,
+    dataProcessing: false,
+
+  }).done(success)
+    .fail(failure);
+};
+
+const signOut = (success, failure) => {
+  // if (!app.user) bad;
+  $.ajax({
+    method: 'DELETE',
+    url: app.api + '/sign-out/' + app.user.id,
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+  }).done(success)
+  .fail(failure);
+};
