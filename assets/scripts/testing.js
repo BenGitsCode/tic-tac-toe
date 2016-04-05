6035b8f3409aaf07f59c87d419b21233424cb658
“Benjamski” <your_email@example.com>
2016-04-05 17:24:26 -0400

    Game.js
    some game logic sketched out

--- assets/scripts/testing.js
+++ assets/scripts/testing.js
@@ -1,30 +1,36 @@
 'use strict';
 
-// win conditions
-let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
- let x = "xzibit";
- let o = "oprah";
- //Checks to see if either X or O has hit all the winning combinations
 
- let getWinner = function(player){
-   var winner = false;
-   if (
-       //Wins Horizontal
-       (player === board[0] && player === board[1] && player === board [2]) ||
-       (player === board[3] && player === board[4] && player === board [5]) ||
-       (player === board[6] && player === board[7] && player === board [8]) ||
+let displayChars = ['X', 'O'];
+let board = ['', '', '', '', '', '', '', '', ''];
+let xWins = 0;
+let oWins = 0;
+let oPlayerImg;
+let xPlayerImg;
 
-       //Wins Vertically
-       (player === board[0] && player === board[3] && player === board [6]) ||
-       (player === board[1] && player === board[4] && player === board [7]) ||
-       (player === board[2] && player === board[5] && player === board [8]) ||
+// all potential win possibilities
 
-       //Wins Diagonally
-       (player === board[0] && player === board[4] && player === board [8]) ||
-       (player === board[2] && player === board[4] && player === board [6])) {
 
-     winner = true;
-       // console.log(winner 'is the winner');
-     }
-     return winner;
+let getWinner = function(player){
+  let winner = false;
+  if (
+      //Wins Horizontal
+      (player === board[0] && player === board[1] && player === board [2]) ||
+      (player === board[3] && player === board[4] && player === board [5]) ||
+      (player === board[6] && player === board[7] && player === board [8]) ||
+
+      //Wins Vertically
+      (player === board[0] && player === board[3] && player === board [6]) ||
+      (player === board[1] && player === board[4] && player === board [7]) ||
+      (player === board[2] && player === board[5] && player === board [8]) ||
+
+      //Wins Diagonally
+      (player === board[0] && player === board[4] && player === board [8]) ||
+      (player === board[2] && player === board[4] && player === board [6])) {
+
+    winner = true;
+    console.log(player 'is the winner');
+    }
+    return winner;
+  }
