b4029ec8b200032f6d37571cd2fe1980b3249941
“Benjamski” <your_email@example.com>
2016-04-06 14:38:36 -0400

    Moved turnclick++ to bottom of both functions

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,6 +13,8 @@ let turnClick = 0;
 // let xPlayerImg;
 let playerIcons = ["Oprah", "Xzibit"];
 let playerIcon = "";
+// let oprahImg = "";
+// let xzibitImg = "";
 
 let board = [$("#C1"),$("#C2"),$("#C3"),$("#C4"),$("#C5"),$("#C6"),$("#C7"),$("#C8"),$("#C9")];
 
@@ -36,59 +38,127 @@ let winArray = [
   rightDiagWin,
 ];
 
-// takes num of clicks % 2
+// takes num of clicks % 2  to determine player icon
 function getPlayer(numOfClicks){
   if (numOfClicks % 2 === 0) {
   playerIcon = playerIcons[0];
 } else {
-playerIcon = playerIcons[1];
+ playerIcon = playerIcons[1];
 }
+
 }
 
-
-
-
-
-//listens for click event on any td
-// puts player icon in currentCell (clicked cell)
+// THIS WORKS by assigning class changes with color background changes of
+// red and blue to players x and o
 
 $('td').on('click', function(){
-  turnClick++;
-  if (turnClick % 2 === 0) {
-  playerIcon = playerIcons[0];
-} else {
-playerIcon = playerIcons[1];
-}
-  console.log(playerIcon);
-  console.log(turnClick);
+    if (turnClick % 2 === 0) {
+    playerIcon = playerIcons[0];
+    } else {
+    playerIcon = playerIcons[1];
+  }
   let currentCell = $(this);
-  currentCell.text(playerIcon);
-  console.log(currentCell.id);
-  //checks move validity
-  currentCell.text(playerIcon);
-});
-
-
-
-let checkWin = function(winArray) {
-  let win = false;
-  if (turnClick >= 4) {
-
+  if (currentCell.hasClass('start-cell')) {
+    console.log(currentCell.attr('class'));
+    currentCell.removeClass('start-cell');
+    console.log(currentCell.attr('class'));
+    currentCell.addClass('playerIcon');
+    console.log(currentCell.attr('class'));
+    console.log((playerIcon));
+  if (playerIcon === 'Xzibit') {     //should this be playerIcons[0] instead of "xzibit"?
+    currentCell.addClass('xzibit');
+  }else {
+    currentCell.addClass('oprah');
+    }
+    turnClick++;
   }
-  for (var i = 0; i < board.length; i++) {
-    array[i];
+  }
+);
+
+
+
+// listens for click event on td, sets clicked cell to currentCell and inserts
+// string of current player
+// THIS VERSION WORKS
+// it assigns string names to player X and O
+
+// $('td').on('click', function(){
+//   if (turnClick % 2 === 0) {
+//   playerIcon = playerIcons[0];
+// } else {
+// playerIcon = playerIcons[1];
+// }
+// let currentCell = $(this);
+//   console.log(playerIcon);
+//   console.log(turnClick);
+//   currentCell.text(playerIcon);
+//   console.log(currentCell.attr('id')); //logs which cell is clicked by it's id
+//   console.log(currentCell.attr('class')); // logs the class of clicked cell
+//   //checks move validity
+//   currentCell.text(playerIcon);
+//    turnClick++;
+// });
+
+
+
+//testing different winner function
+function getWinner(playerIcon) {
+  let winner = false;
+  if (
+      //Wins ROWS
+      (playerIcon === $("#C1") && playerIcon === $("#C2") && playerIcon === $("#C3")) ||
+      (playerIcon === $("#C4") && playerIcon === $("#C5") && playerIcon === $("#C6")) ||
+      (playerIcon === $("#C6") && playerIcon === $("#C7") && playerIcon === $("#C8")) ||
+
+      //Wins COLUMNS
+      (playerIcon === $("#C1") && playerIcon === $("#C3") && playerIcon === $("#C")) ||
+      (playerIcon === $("#C1") && playerIcon === $("#C4") && playerIcon === $("#C")) ||
+      (playerIcon === $("#C2") && playerIcon === $("#C5") && playerIcon === $("#C")) ||
+
+      //Wins DIAGONALLY
+      (playerIcon === $("#C1") && playerIcon === $("#C4") && playerIcon === $("#C"))||
+      (playerIcon === $("#C2") && playerIcon === $("#C4") && playerIcon === $("#C")))
+
+      {
+
+    winner = true;
+      // console.log(winner 'is the winner');
+    }
+    return winner;
+    console.log(winner + " wins!");
   }
 
-};
+
+
+
+
+
+
+
+
+
+
+
+
+// let checkWin = function(playerIcon) {
+//   let win = false;
+//   if (turnClick >= 4) {
+//
+//   }
+//   for (var i = 0; i < board.length; i++) {
+//     if (winArray = true)
+//   }
+//
+// };
 
 // checks for a tie
-function isTie(){
-  var tie = false;
-  if (turnClick >= 9){
-    tie = true;
-    }
-    return tie;
-  }
+// function isTie(){
+//   var tie = false;
+//   if (turnClick >= 9){
+//     tie = true;
+//     }
+//     return tie;
+//   }
 
 
 
