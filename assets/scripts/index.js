df2e9b28fef9fca59ba31e77a06694302ab2545d
“Benjamski” <your_email@example.com>
2016-04-07 00:10:47 -0400

    images not yet working
    on right track?

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -18,8 +18,8 @@ let turnClick = 0;
 // let xPlayerImg;
 let playerIcons = ["Oprah", "Xzibit"];
 let playerIcon = "";
-// let oprahImg = "";
-// let xzibitImg = "";
+let oPlayerImg = "assets/images/oprah1.gif";
+let xPlayerImg = "assets/images/xzibit.gif";
 
 let board = ["", "", "", "", "", "", "", "", "" ];
 
@@ -52,6 +52,34 @@ let getWinner = function(board) {
 
 
 
+    // $('td').on('click', function(){
+    //     if (turnClick % 2 === 0) {
+    //     playerIcon = playerIcons[0];
+    //     } else {
+    //     playerIcon = playerIcons[1];
+    //   }
+    //   let currentCell = $(this);
+    //   if (currentCell.hasClass('start-cell')) {
+    //     console.log(currentCell.attr('class'));
+    //     currentCell.removeClass('start-cell');
+    //     console.log(currentCell.attr('class'));
+    //     currentCell.addClass('playerIcon');
+    //     console.log(currentCell.attr('class'));
+    //     console.log((playerIcon));
+    //   if (playerIcon === 'Xzibit') {     //should this be playerIcons[0] instead of "xzibit"?
+    //     currentCell.removeClass()
+    //     currentCell.addClass('xzibit');
+    //   }else {
+    //     currentCell.addClass('oprah');
+    //     }
+    //     turnClick++;
+    //     getWinner(board);
+    //     isTie(board);
+    //   }
+    //   }
+    // );
+
+
     $('td').one('click', function(){
       if (turnClick % 2 === 0) {
         playerIcon = playerIcons[0];
@@ -63,6 +91,9 @@ let getWinner = function(board) {
       console.log(turnClick);
       currentCell.text(playerIcon);
       $(this).data('board', playerIcon);   //This is what logs playerIcon to board array
+
+      // right idea for inserting img into html??
+      $(currentCell.html('<img src=" + xPlayerImg + >');
       let attrId = $(this).attr('id');
       board[attrId] = playerIcon;
       console.log(currentCell.attr('id')); //logs which cell is clicked by it's id
@@ -75,75 +106,3 @@ let getWinner = function(board) {
       isTie(board);
     });
   });
-
-
-
-
-
-
-// let topRowWin = [$("#C1"),$("#C2"),$("#C3")];
-// let centerRowWin = [$("#C4"),$("#C5"),$("#C6")];
-// let botRowWin = [$("#C7"),$("#C8"),$("#C9")];
-// let topColWin = [$("#C1"),$("#C4"),$("#C7")];
-// let centerColWin = [$("#C2"),$("#C5"),$("#C8")];
-// let botColWin = [$("#C3"),$("#C6"),$("#C9")];
-// let leftDiagWin = [$("#C1"),$("#C5"),$("#C9")];
-// let rightDiagWin = [$("#C3"),$("#C5"),$("#C7")];
-//
-// let winArray = [
-//   topRowWin,
-//   centerRowWin,
-//   botRowWin,
-//   topColWin,
-//   centerColWin,
-//   botColWin,
-//   leftDiagWin,
-//   rightDiagWin,
-// ];
-
-// takes num of clicks % 2  to determine player icon
-// function getPlayer(numOfClicks){
-//   if (numOfClicks % 2 === 0) {
-//   playerIcon = playerIcons[0];
-// } else {
-//  playerIcon = playerIcons[1];
-// }
-// }
-
-
-
-
-// THIS WORKS by assigning class changes with color background changes of
-// red and blue to players x and o
-// does not work for css images yet
-
-// $('td').on('click', function(){
-//     if (turnClick % 2 === 0) {
-//     playerIcon = playerIcons[0];
-//     } else {
-//     playerIcon = playerIcons[1];
-//   }
-//   let currentCell = $(this);
-//   if (currentCell.hasClass('start-cell')) {
-//     console.log(currentCell.attr('class'));
-//     currentCell.removeClass('start-cell');
-//     console.log(currentCell.attr('class'));
-//     currentCell.addClass('playerIcon');
-//     console.log(currentCell.attr('class'));
-//     console.log((playerIcon));
-//   if (playerIcon === 'Xzibit') {     //should this be playerIcons[0] instead of "xzibit"?
-//     currentCell.addClass('xzibit');
-//   }else {
-//     currentCell.addClass('oprah');
-//     }
-//     turnClick++;
-//   }
-//   }
-// );
-
-
-
-// listens for click event on td, sets clicked cell to currentCell and inserts
-// string of current player
-// THIS VERSION WORKS
-// it assigns string names to player X and O
