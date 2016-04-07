93501e14bfe75405699f068cb757e994875b9414
“Benjamski” <your_email@example.com>
2016-04-07 09:35:14 -0400

    Tried to reorganize gamelogic into new file: game.js
    uncaught type error that made no sense
    
    was reading first index of getWinner statement as integer?

---
+++ assets/scripts/game.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const authEvents = require('./auth/events.js');
+
+
+
+
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
+
+// use require without a reference to ensure a file is bundled
+
+
+// require('assets/scripts/index.js');
+
+let turnClick = 0;
+// let oPlayerImg;
+// let xPlayerImg;
+let playerIcons = ["Oprah", "Xzibit"];
+let playerIcon = "";
+// let oprahImg = "";
+// let xzibitImg = "";
+let win = false;
+let board = ["", "", "", "", "", "", "", "", "" ];
+
+
+const getWinner = function(board) {
+   if (playerIcon === board[0] && playerIcon === board[1] && playerIcon === board[2] ||
+       playerIcon === board[3] && playerIcon === board[4] && playerIcon === board[5] ||
+       playerIcon === board[6] && playerIcon === board[7] && playerIcon === board[8] ||
+      //  row wins ^
+       playerIcon === board[0] && playerIcon === board[3] && playerIcon === board[6] ||
+       playerIcon === board[1] && playerIcon === board[4] && playerIcon === board[7] ||
+       playerIcon === board[2] && playerIcon === board[5] && playerIcon === board[8] ||
+      //  col wins ^
+       playerIcon === board[0] && playerIcon === board[4] && playerIcon === board[8] ||
+       playerIcon === board[2] && playerIcon === board[4] && playerIcon === board[6]){
+        //  diag wins
+
+      console.log("winner is " + playerIcon);
+      win = true;
+      $('td').off('click');
+    } else{
+    }
+  };
+  // checks for a tie
+  let isTie = function() {
+    let tie = false;
+    if (turnClick === 9 && win === false){
+      console.log("It's a tie ya'll!");
+      tie = true;
+      }
+    };
+
+
+
+    $('td').one('click', function(){
+      if (turnClick % 2 === 0) {
+        playerIcon = playerIcons[0];
+      } else {
+        playerIcon = playerIcons[1];
+      }
+      let currentCell = $(this);
+      turnClick++;
+        console.log(playerIcon);
+        console.log("turnclicks current" + turnClick);
+        currentCell.text(playerIcon);
+        $(this).data('board', playerIcon);   //This is what logs playerIcon to board array
+        let attrId = $(this).attr('id');
+        board[attrId] = playerIcon;
+        console.log("cell " + currentCell.attr('id')); //logs which cell is clicked by it's id
+        console.log("cell class " + currentCell.attr('class')); // logs the class of clicked cell
+        console.log(board);
+        currentCell.text(playerIcon);
+      getWinner(board);
+      isTie(turnClick);
+    });
+
+    module.exports = {
+      getWinner,
+    };
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,147 +1,11 @@
 'use strict';
 
 const authEvents = require('./auth/events.js');
+const game = require('./game.js');
 $(() => {
 
 authEvents.signcheck();
-
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
-
-// use require without a reference to ensure a file is bundled
-
-
-// require('assets/scripts/index.js');
-
-
-
-let turnClick = 0;
-// let oPlayerImg;
-// let xPlayerImg;
-let playerIcons = ["O", "X"];
-let playerIcon = "";
-// let oprahImg = "";
-// let xzibitImg = "";
-
-let board = ['', '', '', '', '', '', '', '', '', ];
-
-
-let topRowWin = [$("#C1"),$("#C2"),$("#C3")];
-let centerRowWin = [$("#C4"),$("#C5"),$("#C6")];
-let botRowWin = [$("#C7"),$("#C8"),$("#C9")];
-let topColWin = [$("#C1"),$("#C4"),$("#C7")];
-let centerColWin = [$("#C2"),$("#C5"),$("#C8")];
-let botColWin = [$("#C3"),$("#C6"),$("#C9")];
-let leftDiagWin = [$("#C1"),$("#C5"),$("#C9")];
-let rightDiagWin = [$("#C3"),$("#C5"),$("#C7")];
-
-let winArray = [
-  topRowWin,
-  centerRowWin,
-  botRowWin,
-  topColWin,
-  centerColWin,
-  botColWin,
-  leftDiagWin,
-  rightDiagWin,
-];
-
-// takes num of clicks % 2  to determine player icon
-function getPlayer(numOfClicks){
-  if (numOfClicks % 2 === 0) {
-  playerIcon = playerIcons[0];
-} else {
- playerIcon = playerIcons[1];
-
-}
-
-}
-
-// THIS WORKS by assigning class changes with color background changes of
-// red and blue to players x and o
-// does not work for css images yet
-
-// $('td').on('click', function(){
-//     if (turnClick % 2 === 0) {
-//     playerIcon = playerIcons[0];
-//     } else {
-//     playerIcon = playerIcons[1];
-//   }
-//   let currentCell = $(this);
-//   if (currentCell.hasClass('start-cell')) {
-//     console.log(currentCell.attr('class'));
-//     currentCell.removeClass('start-cell');
-//     console.log(currentCell.attr('class'));
-//     currentCell.addClass('playerIcon');
-//     console.log(currentCell.attr('class'));
-//     console.log((playerIcon));
-//   if (playerIcon === 'Xzibit') {     //should this be playerIcons[0] instead of "xzibit"?
-//     currentCell.addClass('xzibit');
-//   }else {
-//     currentCell.addClass('oprah');
-//     }
-//     turnClick++;
-//   }
-//   }
-// );
-
-
-
-// listens for click event on td, sets clicked cell to currentCell and inserts
-// string of current player
-// THIS VERSION WORKS
-// it assigns string names to player X and O
-
-$('td').on('click', function(){
-  if (turnClick % 2 === 0) {
-  playerIcon = playerIcons[0];
-} else {
-playerIcon = playerIcons[1];
-}
-let currentCell = $(this);
-  console.log(playerIcon);
-  console.log(turnClick);
-  currentCell.text(playerIcon);
-  $(this).data('populatedCell', playerIcon);
-  var attrId = $(this).attr('id');
-  board[attrId] = playerIcon;
-  console.log(board);
-  console.log(currentCell.attr('id')); //logs which cell is clicked by it's id
-  console.log(currentCell.attr('class')); // logs the class of clicked cell
-  //checks move validity
-  currentCell.text(playerIcon);
-   turnClick++;
-});
-
-
-
-// //testing different winner function
-// function getWinner(playerIcon) {
-//   let winner = false;
-//   if (
-//       //Wins ROWS
-//       (playerIcon === $("#C1") && playerIcon === $("#C2") && playerIcon === $("#C3")) ||
-//       (playerIcon === $("#C4") && playerIcon === $("#C5") && playerIcon === $("#C6")) ||
-//       (playerIcon === $("#C6") && playerIcon === $("#C7") && playerIcon === $("#C8")) ||
-//
-//       //Wins COLUMNS
-//       (playerIcon === $("#C1") && playerIcon === $("#C3") && playerIcon === $("#C")) ||
-//       (playerIcon === $("#C1") && playerIcon === $("#C4") && playerIcon === $("#C")) ||
-//       (playerIcon === $("#C2") && playerIcon === $("#C5") && playerIcon === $("#C")) ||
-//
-//       //Wins DIAGONALLY
-//       (playerIcon === $("#C1") && playerIcon === $("#C4") && playerIcon === $("#C"))||
-//       (playerIcon === $("#C2") && playerIcon === $("#C4") && playerIcon === $("#C")))
-//
-//       {
-//
-//     winner = true;
-//       // console.log(winner 'is the winner');
-//     }
-//     console.log(winner + " wins!");
-//     return winner;
-//   }
-//
+game.getWinner();
 
 
 
@@ -151,39 +15,6 @@ let currentCell = $(this);
 
 
 
-// checks for a tie
-function isTie(){
-  let tie = false;
-  if (turnClick > 9){
-    tie = true;
-    }
-    console.log("It's a tie ya'll!");
-    return tie;
-  }
-
-
-
-
-
-// let checkWin = function(playerIcon) {
-//   let win = false;
-//   if (turnClick >= 4) {
-//
-//   }
-//   for (var i = 0; i < board.length; i++) {
-//     if (winArray = true)
-//   }
-//
-// };
-
-// checks for a tie
-// function isTie(){
-//   var tie = false;
-//   if (turnClick >= 9){
-//     tie = true;
-//     }
-//     return tie;
-//   }
 
 
 });
