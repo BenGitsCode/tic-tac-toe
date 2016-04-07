1f7eb0783b7542ba7a3535e0b45e2d1bb51e60b0
“Benjamski” <your_email@example.com>
2016-04-06 23:30:56 -0400

    moved some snippets to graveyard to  clean up index .js
    indented some of my click function code in order to clean it up some
    
    still not getting tie to print to console

--- assets/scripts/graveyard.js
+++ assets/scripts/graveyard.js
@@ -70,3 +70,67 @@
 //      return false;
 //  }
 // };
+
+
+// DUPLICATES????
+
+
+// let topRowWin = [$("#C1"),$("#C2"),$("#C3")];
+// let centerRowWin = [$("#C4"),$("#C5"),$("#C6")];
+// let botRowWin = [$("#C7"),$("#C8"),$("#C9")];
+// let topColWin = [$("#C1"),$("#C4"),$("#C7")];
+// let centerColWin = [$("#C2"),$("#C5"),$("#C8")];
+// let botColWin = [$("#C3"),$("#C6"),$("#C9")];
+// let leftDiagWin = [$("#C1"),$("#C5"),$("#C9")];
+// let rightDiagWin = [$("#C3"),$("#C5"),$("#C7")];
+//
+// let winArray = [
+//   topRowWin,
+//   centerRowWin,
+//   botRowWin,
+//   topColWin,
+//   centerColWin,
+//   botColWin,
+//   leftDiagWin,
+//   rightDiagWin,
+// ];
+
+// takes num of clicks % 2  to determine player icon
+// function getPlayer(numOfClicks){
+//   if (numOfClicks % 2 === 0) {
+//   playerIcon = playerIcons[0];
+// } else {
+//  playerIcon = playerIcons[1];
+// }
+// }
+
+
+
+
+// THIS WORKS by assigning class changes with color background changes of
+// red and blue to players x and o
+// does not work for css images yet
+
+// $('td').on('click', function(){
+//     if (turnClick % 2 === 0) {
+//     playerIcon = playerIcons[0];
+//     } else {
+//     playerIcon = playerIcons[1];
+//   }
+//   let currentCell = $(this);
+//   if (currentCell.hasClass('start-cell')) {
+//     console.log(currentCell.attr('class'));
+//     currentCell.removeClass('start-cell');
+//     console.log(currentCell.attr('class'));
+//     currentCell.addClass('playerIcon');
+//     console.log(currentCell.attr('class'));
+//     console.log((playerIcon));
+//   if (playerIcon === 'Xzibit') {     //should this be playerIcons[0] instead of "xzibit"?
+//     currentCell.addClass('xzibit');
+//   }else {
+//     currentCell.addClass('oprah');
+//     }
+//     turnClick++;
+//   }
+//   }
+// );
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -39,14 +39,13 @@ let getWinner = function(board) {
       $('td').off('click');
     } else{
     }
-    console.log("Its a tie");
   };
   // checks for a tie
   function isTie(){
     let tie = false;
-    if (turnClick >= 9 && getWinner === false) {
+    if (turnClick >= 9 && getWinner === false){
+      console.log("It's a tie ya'll!");
       tie = true;
-      console.log("Its a tie ya'll!");
       }
     }
 
@@ -59,91 +58,19 @@ let getWinner = function(board) {
         playerIcon = playerIcons[1];
       }
       let currentCell = $(this);
-      console.log(playerIcon);
-      console.log(turnClick);
-      currentCell.text(playerIcon);
-      $(this).data('board', playerIcon);   //This is what logs playerIcon to board array
-      let attrId = $(this).attr('id');
-      board[attrId] = playerIcon;
-      console.log(currentCell.attr('id')); //logs which cell is clicked by it's id
-      console.log(currentCell.attr('class')); // logs the class of clicked cell
-      console.log(board);
-      //checks move validity
-      currentCell.text(playerIcon);
-      turnClick++;
+        console.log(playerIcon);
+        console.log(turnClick);
+        currentCell.text(playerIcon);
+        $(this).data('board', playerIcon);   //This is what logs playerIcon to board array
+        let attrId = $(this).attr('id');
+        board[attrId] = playerIcon;
+        console.log(currentCell.attr('id')); //logs which cell is clicked by it's id
+        console.log(currentCell.attr('class')); // logs the class of clicked cell
+        console.log(board);
+        //checks move validity
+        currentCell.text(playerIcon);
+        turnClick++;
       getWinner(board);
       isTie(board);
     });
   });
-
-
-
-
-
-
-// let topRowWin = [$("#C1"),$("#C2"),$("#C3")];
-// let centerRowWin = [$("#C4"),$("#C5"),$("#C6")];
-// let botRowWin = [$("#C7"),$("#C8"),$("#C9")];
-// let topColWin = [$("#C1"),$("#C4"),$("#C7")];
-// let centerColWin = [$("#C2"),$("#C5"),$("#C8")];
-// let botColWin = [$("#C3"),$("#C6"),$("#C9")];
-// let leftDiagWin = [$("#C1"),$("#C5"),$("#C9")];
-// let rightDiagWin = [$("#C3"),$("#C5"),$("#C7")];
-//
-// let winArray = [
-//   topRowWin,
-//   centerRowWin,
-//   botRowWin,
-//   topColWin,
-//   centerColWin,
-//   botColWin,
-//   leftDiagWin,
-//   rightDiagWin,
-// ];
-
-// takes num of clicks % 2  to determine player icon
-// function getPlayer(numOfClicks){
-//   if (numOfClicks % 2 === 0) {
-//   playerIcon = playerIcons[0];
-// } else {
-//  playerIcon = playerIcons[1];
-// }
-// }
-
-
-
-
-// THIS WORKS by assigning class changes with color background changes of
-// red and blue to players x and o
-// does not work for css images yet
-
-// $('td').on('click', function(){
-//     if (turnClick % 2 === 0) {
-//     playerIcon = playerIcons[0];
-//     } else {
-//     playerIcon = playerIcons[1];
-//   }
-//   let currentCell = $(this);
-//   if (currentCell.hasClass('start-cell')) {
-//     console.log(currentCell.attr('class'));
-//     currentCell.removeClass('start-cell');
-//     console.log(currentCell.attr('class'));
-//     currentCell.addClass('playerIcon');
-//     console.log(currentCell.attr('class'));
-//     console.log((playerIcon));
-//   if (playerIcon === 'Xzibit') {     //should this be playerIcons[0] instead of "xzibit"?
-//     currentCell.addClass('xzibit');
-//   }else {
-//     currentCell.addClass('oprah');
-//     }
-//     turnClick++;
-//   }
-//   }
-// );
-
-
-
-// listens for click event on td, sets clicked cell to currentCell and inserts
-// string of current player
-// THIS VERSION WORKS
-// it assigns string names to player X and O
