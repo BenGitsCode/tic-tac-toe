941a2d308b636e2daa8d68dc0e1185b2131de65b
“Benjamski” <your_email@example.com>
2016-04-06 21:39:33 -0400

    moved console.log of it's a tie
    was being printed tie or not

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -45,8 +45,8 @@ let getWinner = function(board) {
     let tie = false;
     if (turnClick >= 9){
       tie = true;
-      }
       console.log("It's a tie ya'll!");
+      }
       return tie;
     }
 
@@ -146,95 +146,4 @@ $('td').one('click', function(){
      getWinner(board);
      isTie(board);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
-
-// let checkWin = function(playerIcon) {
-//   let win = false;
-//   if (turnClick >= 4) {
-//
-//   }
-//   for (var i = 0; i < board.length; i++) {
-//     if (winArray = true)
-//   }
-//
-// };
-
-// checks for a tie
-// function isTie(){
-//   var tie = false;
-//   if (turnClick >= 9){
-//     tie = true;
-//     }
-//     return tie;
-//   }
-
-
-
-
-
-
-
-
-
-// //testing different winner function
-// function getWinner(playerIcon) {
-//   let winner = false;
-//   if (
-//       //Wins ROWS
-//       (playerIcon === $("#C1") && playerIcon === $("#C2") && playerIcon === $("#C3")) ||
-//       (playerIcon === $("#C4") && playerIcon === $("#C5") && playerIcon === $("#C6")) ||
-//       (playerIcon === $("#C6") && playerIcon === $("#C7") && playerIcon === $("#C8")) ||
-//
-//       //Wins COLUMNS
-//       (playerIcon === $("#C1") && playerIcon === $("#C3") && playerIcon === $("#C")) ||
-//       (playerIcon === $("#C1") && playerIcon === $("#C4") && playerIcon === $("#C")) ||
-//       (playerIcon === $("#C2") && playerIcon === $("#C5") && playerIcon === $("#C")) ||
-//
-//       //Wins DIAGONALLY
-//       (playerIcon === $("#C1") && playerIcon === $("#C4") && playerIcon === $("#C"))||
-//       (playerIcon === $("#C2") && playerIcon === $("#C4") && playerIcon === $("#C")))
-//
-//       {
-//
-//     winner = true;
-//       // console.log(winner 'is the winner');
-//     }
-//     console.log(winner + " wins!");
-//     return winner;
-//   }
-
-// let winner = function(playerIcon) {
-//   let win = false;
-//    if (playerIcon === board[0] && playerIcon === board[1] && playerIcon === board[2] ||
-//        playerIcon === board[3] && playerIcon === board[4] && playerIcon === board[5] ||
-//        playerIcon === board[6] && playerIcon === board[7] && playerIcon === board[8] ||
-//        playerIcon === board[0] && playerIcon === board[3] && playerIcon === board[6] ||
-//        playerIcon === board[1] && playerIcon === board[4] && playerIcon === board[7] ||
-//        playerIcon === board[2] && playerIcon === board[5] && playerIcon === board[8] ||
-//        playerIcon === board[0] && playerIcon === board[4] && playerIcon === board[8] ||
-//        playerIcon === board[2] && playerIcon === board[4] && playerIcon === board[6]){
-//
-//      return win = true;
-//      console.log("winner is" + playerIcon);
-//    } else{
-//      return false;
-//  }
-// };
